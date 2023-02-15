652562848ff0584724aa26aef6987f18687eb186
nahwasa <nahwasa@gmail.com>
2023-02-15 20:28:44 +0900

    race condition이 발생해 테스트가 실패하도록 함

--- src/test/java/com/nahwasa/study/howtosolveconcurrencyissues/service/StockServiceTest.java
+++ src/test/java/com/nahwasa/study/howtosolveconcurrencyissues/service/StockServiceTest.java
@@ -10,6 +10,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 class StockServiceTest {
 
@@ -35,6 +41,33 @@ class StockServiceTest {
 
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
-        Assertions.assertThat(stock.getQuantity()).isEqualTo(99L);
+        assertThat(stock.getQuantity()).isEqualTo(99L);
+    }
+
+    @Test
+    @DisplayName("quantity 100에서 동시에 100개의 요청을 해 1씩 감소하면 quantity는 0이어야 한다.")
+    void stock_decrease_concurrency() throws InterruptedException {
+        /**
+         * race condition 발생하므로 테스트는 실패한다.
+         */
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertThat(stock.getQuantity()).isEqualTo(0L);
     }
 }
