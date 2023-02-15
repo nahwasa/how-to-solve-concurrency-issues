604b8c21f5a30e1b27abb270131c8dd8efb58e90
nahwasa <nahwasa@gmail.com>
2023-02-15 19:59:39 +0900

    initializr

---
+++ src/main/java/com/nahwasa/study/howtosolveconcurrencyissues/HowToSolveConcurrencyIssuesApplication.java
@@ -0,0 +1,13 @@
+package com.nahwasa.study.howtosolveconcurrencyissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToSolveConcurrencyIssuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowToSolveConcurrencyIssuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nahwasa/study/howtosolveconcurrencyissues/HowToSolveConcurrencyIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahwasa.study.howtosolveconcurrencyissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowToSolveConcurrencyIssuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
